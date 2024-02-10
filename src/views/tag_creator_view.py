57e6519c6ddd5cc5bc0325267e71180ef160ee78
Filipe Piazza <filipepiazza@gmail.com>
2024-02-10 19:15:47 -0300

    feat:implement controller and error handler

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,5 +1,6 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controller.tag_creator_controller import TagCreatorController
 
 class TagCreatorView:
     '''
@@ -9,8 +10,10 @@ class TagCreatorView:
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
         body = http_request.body
         product_code = body["product_code"]
-        print(product_code)
+
         #logica de regra de negocio
+        tag_creator_controller = TagCreatorController()
+        formatted_response = tag_creator_controller.create(product_code)
 
         # retorno http
-        return HttpResponse(status_code=200, body={"hello":"hello"})
+        return HttpResponse(status_code=200, body=formatted_response)
